5afa64ab2c1fa7e4e43f09ad83089b353af1bd98
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 15:40:01 +0100

    träning hur man kan gör check om Array och hur kan det ser ut med if och else

---
+++ main.js
@@ -0,0 +1,93 @@
+/* global console, alert, prompt*/
+
+/*
+var friendOne = "Hassan",
+
+    friendTwo = "Soha",
+
+    friendThree = "Ahmad",
+
+    friendFour = "Zahra",
+
+    friendFive = "Hind";
+
+var friends = [
+    "Hassan", // 0
+    "Soha",   // 1
+    "Ahmad",  // 2
+    "Zahra",  // 3
+    "Hind"    // 4
+];
+
+console.log(friends);
+
+console.log(friends[1]);
+
+if ( Array.isArray(friendOne)){
+    console.log("Good This Is Array");
+
+}else{
+    console.log("No This Is Array")
+}
+*/
+/*
+var friendOne = "Hassan",
+
+    friendTwo = "Soha",
+
+    friendThree = "Ahmad",
+
+    friendFour = "Zahra",
+
+    friendFive = "Hind";
+
+var friends = [
+    "Hassan", // 0
+    "Soha",   // 1
+    "Ahmad",  // 2
+    "Zahra",  // 3
+    "Hind"    // 4
+];
+
+console.log(friends);
+
+console.log(friends[3]);
+
+if (friends.constructor === Array) {
+
+    console.log("Good This Is Array");
+
+} else {
+    console.log("No This Is Array")
+}
+*/
+
+var friendOne = "Hassan",
+
+    friendTwo = "Soha",
+
+    friendThree = "Ahmad",
+
+    friendFour = "Zahra",
+
+    friendFive = "Hind";
+
+var friends = [
+    "Hassan", // 0
+    "Soha",   // 1
+    "Ahmad",  // 2
+    "Zahra",  // 3
+    "Hind"    // 4
+];
+
+console.log(friends);
+
+console.log(friends[3]);
+
+if (Array.isArray (friends)) {
+
+    console.log("Good This Is Array");
+
+} else {
+    console.log("No This Is Array")
+}
